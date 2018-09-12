0339d95973f200295a46b22aa039909a7ca50569
tanyun.zheng <tanyun.zheng@luckincoffee.com>
2018-09-12 11:03:36 +0800

    补充异常描述

--- src/main/java/com/labs/basis/exception/ExceptionTest.java
+++ src/main/java/com/labs/basis/exception/ExceptionTest.java
@@ -4,7 +4,21 @@ import com.labs.basis.exception.ex.MyFileNotFoundException;
 import com.labs.basis.exception.utils.Assert;
 
 /**
- * @Description:
+ * @Description: 异常测试
+ * Throwable 类
+ *      + Error  代表了JVM本身的错误。错误不能被程序员通过代码处理
+ *      + Exception 代表程序运行时发送的各种不期望发生的事件
+ *
+ *      - CHECK 除了Error 和 RuntimeException的其它异常，javac强制要求程序员为这样的异常做预备处理工作（使用try…catch…finally或者throws）
+ *      (IOException, EOFException,FileNotFoundException)
+ *
+ *      - UNCHECK Error 和 RuntimeException 以及他们的子类
+ *      ( Error,VitrualMachineError,StackOverFlowError,OutOfMemoryError,AWTError.
+ *        RuntimeException,ArrithmeticException,MissingResourceException,ClassNotFoundException,NullPointerException, IllegalArgumenetException,ArrayIndexOutOfBoundException,UnknownTypeExcception)
+ *
+ *
+ *
+ *
  * @Auther: win10
  * @Date: 2018/9/12 09:15
  */
@@ -29,6 +43,5 @@ public class ExceptionTest {
         ExceptionTest service = new ExceptionTest();
         service.methodWithThrows();
         service.methodWithoutThrows();
-
     }
 }
